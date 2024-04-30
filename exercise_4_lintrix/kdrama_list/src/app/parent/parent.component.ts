5606f24479559e24b3469ff7c735ce5e205998f9
Diana Rose Fidel <fideldianarose@gmail.com>
2024-04-30 20:57:44 +0800

    add array of object type

--- exercise_4_lintrix/kdrama_list/src/app/parent/parent.component.ts
+++ exercise_4_lintrix/kdrama_list/src/app/parent/parent.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Kdrama } from '../../model/kdrama'; // Import the Kdrama type
 
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
-  styleUrl: './parent.component.css'
+  styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
+  title: string;
+  lead_actor: string;
+  lead_actress: string;
+  year_released: number;
 
+  kdrama_library: Kdrama[] = [
+    {
+      title: 'Crash Landing on You',
+      lead_actor: 'Hyun Bin',
+      lead_actress: 'Son Ye-jin',
+      year_released: 2019,
+    },
+    {
+      title: 'Goblin',
+      lead_actor: 'Gong Yoo',
+      lead_actress: 'Kim Go-eun',
+      year_released: 2016,
+    },
+  ];
 }
